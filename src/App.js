21bb0fea0365fe53e17963afcd7e30107f7f0521
unknown <bryankeltonadams@gmail.com>
2020-04-27 14:44:36 -0600

    Initialized react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MarketingIndex from './components/Marketing/index';
+import Game from './components/Game/Game'
+function App() {
+  return (
+    <div className="App">
+     <MarketingIndex/>
+     <Game/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Marketing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header'
+
+
+function MarketingIndex() {
+    return(
+        <Header/>
+    )
+}
+
+export default MarketingIndex
