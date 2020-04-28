0181902969910333237d26bf47ec53163de7c044
unknown <bryankeltonadams@gmail.com>
2020-04-28 16:56:39 -0600

    Lots of code for the game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,46 +1,198 @@
-import React, {useEffect} from 'react'
-import {useRef} from 'react'
-import {axiosWithAuth} from '../../utils/axiosWithAuth'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
+import { axiosWithAuth } from '../../utils/axiosWithAuth'
+import { useState } from 'react'
 
+let roomRectangles = []
 
-const HOOK_SVG =  'm129.03125 63.3125c0-34.914062-28.941406-63.3125-64.519531-63.3125-35.574219 0-64.511719 28.398438-64.511719 63.3125 0 29.488281 20.671875 54.246094 48.511719 61.261719v162.898437c0 53.222656 44.222656 96.527344 98.585937 96.527344h10.316406c54.363282 0 98.585938-43.304688 98.585938-96.527344v-95.640625c0-7.070312-4.640625-13.304687-11.414062-15.328125-6.769532-2.015625-14.082032.625-17.960938 6.535156l-42.328125 64.425782c-4.847656 7.390625-2.800781 17.3125 4.582031 22.167968 7.386719 4.832032 17.304688 2.792969 22.160156-4.585937l12.960938-19.71875v42.144531c0 35.582032-29.863281 64.527344-66.585938 64.527344h-10.316406c-36.714844 0-66.585937-28.945312-66.585937-64.527344v-162.898437c27.847656-7.015625 48.519531-31.773438 48.519531-61.261719zm-97.03125 0c0-17.265625 14.585938-31.3125 32.511719-31.3125 17.929687 0 32.511719 14.046875 32.511719 31.3125 0 17.261719-14.582032 31.3125-32.511719 31.3125-17.925781 0-32.511719-14.050781-32.511719-31.3125zm0 0'
-const HOOK_PATH = new Path2D(HOOK_SVG)
-const SCALE = 0.3
-const OFFSET = 80
-function draw(ctx, location) {
-  ctx.fillStyle = 'deepskyblue'
-  ctx.shadowColor = 'dodgerblue'
-  ctx.shadowBlur = 20  
-  ctx.save()
-  ctx.scale(SCALE, SCALE)  
-  ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET)
-  ctx.fill(HOOK_PATH)
-  ctx.restore()
-}
 function App() {
+  let [playerInfo, setPlayerInfo] = useState()
+  let [playerLocation, setPlayerLocation] = useState([0, 0])
+  let [roomData, setRoomData] = useState()
+  const canvasRef = useRef(null)
+
   useEffect(() => {
     axiosWithAuth().get("https://lambda-mud-test.herokuapp.com/api/adv/init/")
-    .then(res => {
-      console.log(res)
+      .then(res => {
+        setPlayerInfo(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  let newArr = roomRectangles.slice(0)
+  function updateCanvas(x, y, color) {
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.fillRect(x * 100 + 640, y * 100 + 360, 100, 100);
+    ctx.fillStyle = color
+
+  }
+  newArr.map(room => {
+    console.log(playerLocation)
+    if (room[0] == playerLocation[0] && room[1] == playerLocation[1]) {
+      console.log("Player is here")
+      console.log(room[0], room[1])
+      updateCanvas(room[0], room[1], "green")
+    } else if (room[0] != playerLocation[0] && room[1] != playerLocation[1] || room[0] != playerLocation[0] && room[1] == playerLocation[1] || room[0] == playerLocation[0] && room[1] != playerLocation[1]) {
+      console.log("room here")
+      console.log(room[0], room[1])
+      setTimeout(function () {
+        updateCanvas(room[0], room[1], "black")
+
+      }, 100)
+    }
+
+  })
+  useEffect(() => {
+    axiosWithAuth().get("https://lambda-mud-test.herokuapp.com/api/adv/rooms/")
+      .then(res => {
+        setRoomData(JSON.parse(res.data.rooms))
+        makeRoomRectangles(JSON.parse(res.data.rooms))
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  function makeRoomRectangles(roomData) {
+    roomData.sort(function (a, b) {
+      if (a.pk > b.pk) {
+        return 1
+      }
+      return -1
     })
-    .catch(err => {
-      console.log(sessionStorage.getItem('token'))
-      console.log(err)
+    let queue = []
+    let visited = []
+    roomData.map(room => {
+      visited.push(false)
     })
-  },[])
-  const canvasRef = useRef(null)
+    queue.push([roomData[0], 0, 0])
+    visited[0] = true
+    while (queue.length !== 0) {
+      for (let i = 0; i < queue.length; i++) {
+        var node = queue.shift()
+        let x = node[1]
+        let y = node[2]
+        let room = node[0].fields
+        roomRectangles.push([x, y])
+        if (room.n_to != 0 && !visited[room.n_to - 1]) {
+          queue.push([roomData[room.n_to - 1], x, y - 1])
+          console.log(queue.slice(0))
+          visited[room.n_to - 1] = true
+        }
+        if (room.e_to != 0 && !visited[room.e_to - 1]) {
+          queue.push([roomData[room.e_to - 1], x + 1, y])
+          visited[room.e_to - 1] = true
+        }
+        if (room.s_to != 0 && !visited[room.s_to - 1]) {
+          queue.push([roomData[room.s_to - 1], x, y + 1])
+          visited[room.s_to - 1] = true
+        }
+        if (room.w_to != 0 && !visited[room.w_to - 1]) {
+          queue.push([roomData[room.w_to - 1], x - 1, y])
+          visited[room.s_to - 1] = true
+        }
+      }
+    }
+  }
+
+  function postMove(direction) {
+    axiosWithAuth().post("https://lambda-mud-test.herokuapp.com/api/adv/move/", { direction: direction })
+      .then(res => {
+        setPlayerInfo(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  function NClick() {
+    postMove("n")
+  }
+  function EClick() {
+    postMove("e")
+  }
+  function SClick() {
+    postMove("s")
+
+  }
+  function WClick() {
+    postMove("w")
+
+  }
+
+
   return (
-    <canvas
-      style={{border: "1px solid red"}}
-      ref={canvasRef}
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onClick={e => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        draw(context, { x: e.clientX, y: e.clientY })
-      }}
-    />
+    <>
+      {console.log(roomData)
+      }
+      <table>
+        <tr>
+          <td>
+
+          </td>
+          <td>
+            <button onClick={NClick}>
+              ^
+            </button>
+          </td>
+          <td>
+
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button onClick={WClick}>
+              &lt;
+            </button>
+          </td>
+          <td>
+
+          </td>
+          <td>
+            <button onClick={EClick}>
+              &gt;
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+
+          </td>
+          <td>
+            <button onClick={SClick}>
+              v
+            </button>
+          </td>
+          <td>
+
+          </td>
+        </tr>
+
+      </table>
+      <canvas
+        style={{ border: "1px solid red", margin: "auto", width: "50%", display: "block" }}
+        ref={canvasRef}
+        width={"1280px"}
+        height={"720px"}
+      />
+      {playerInfo ? <div>
+        <h1>
+          {playerInfo.title}
+        </h1>
+        <h3>
+          {playerInfo.description}
+        </h3>
+        <h1>
+          Players in the room
+      </h1>
+        {playerInfo.players.map(player => (
+          <p>
+            {player}
+          </p>
+        ))}
+      </div> : null}
+    </>
+
   )
 }
 export default App
